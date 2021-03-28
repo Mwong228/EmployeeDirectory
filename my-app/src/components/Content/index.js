e84e3b3262f0f235468ac621ee424598ba283b72
Michael Wong <michael.w228@gmail.com>
2021-03-28 18:38:51 -0400

    added table to show content

--- my-app/src/components/Content/index.js
+++ my-app/src/components/Content/index.js
@@ -7,26 +7,28 @@ function Content() {
     const [user, setUser] = useState([])
     const [search, setSearch] = useState([])
 
-    useEffect(function (){
+    useEffect(function () {
         init()
     }, [])
-    
-    async function init(){
+
+    async function init() {
         const result = await API.getRandomEmployee()
         setUser(result.data.results)
         console.log(`[init] url fetch`, result.data.results)
     }
 
-    function searchEmployee(){
+    function searchEmployee() {
         console.log('Searching for:', search)
+        // const searchedUser = user.filter(filtered => search.toLowerCase().indexOf(filtered.name.first.toLowerCase())> -1 || search.toLowerCase().indexOf(filtered.name.last.toLowerCase())> -1)
+        // console.log(searchedUser)
     }
 
-    function handleFormSubmit(event){
+    function handleFormSubmit(event) {
         event.preventDefault()
         searchEmployee()
     }
 
-    function handleInputChange(event){
+    function handleInputChange(event) {
         setSearch(event.target.value)
     }
 
@@ -34,7 +36,31 @@ function Content() {
 
     return (
         <>
-            <Search handleFormSubmit={handleFormSubmit} handleInputChange={handleInputChange}/>
+            <Search value={search} handleFormSubmit={handleFormSubmit} handleInputChange={handleInputChange} />
+            <div class="table-responsive">
+                <table class="table table-striped table-hover">
+                    <thread>
+                        <tr>
+                            <th>Image</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Phone</th>
+                            <th>Email</th>
+                        </tr>
+                    </thread>
+                    <tbody>
+                        {user.map(user =>
+                        <tr>
+                            <td><img src={user.picture.thumbnail} alt="pic"/></td>
+                            <td>{user.name.first}</td>
+                            <td>{user.name.last}</td>
+                            <td>{user.phone}</td>
+                            <td>{user.email}</td>
+                        </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
         </>
     )
 
